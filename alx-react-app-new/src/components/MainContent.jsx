219193640694311897bc87af430d24b0918959a9
Osho Emmanuel <oshoemmanuel3@gmail.com>
2026-02-11 19:45:10 +0100

    Created a counter app

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,10 @@
 function MainContent() {
     return (
-        <main 
-        style= {{
+        <main style = {
+            {
             fontFamily: "Arial, sans-serif",
-            fontSize: "10px",}}>
+            fontSize: "10px",}
+            }>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
