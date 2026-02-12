265b3bf6d8ea719acaf54b0f312bb34d236017f4
Osho Emmanuel <oshoemmanuel3@gmail.com>
2026-02-12 18:00:29 +0100

    New Commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
         backgroundColor: '#333',
         display: 'flex',
         gap: '15px',
+        justifyContent: 'center',
       }}
     >
       <Link style={{ color: '#fff' }} to="/">Home</Link>
