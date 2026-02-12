9dbcb32de9a15139a73914f21ad6ca2f2b28ab05
Osho Emmanuel <oshoemmanuel3@gmail.com>
2026-02-12 17:54:53 +0100

    My first SPA in React

---
+++ my-company/src/Pages/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: '40px',
+        padding: '10px',
+        backgroundColor: '#f1f1f1',
+        textAlign: 'center',
+      }}
+    >
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
