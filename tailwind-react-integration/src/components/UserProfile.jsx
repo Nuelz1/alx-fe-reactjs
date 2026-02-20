6347e2934908286b46bae0d01aae785eac9ee5b0
Osho Emmanuel <oshoemmanuel3@gmail.com>
2026-02-20 14:29:48 +0100

    Added page responsiveness for smaller screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img className=" rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto" src="this.png" alt="User" />
-      <h1 className="md:text-xl text-lg text-blue-800 my-4 ">John Doe</h1>
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src="this.png" alt="User" />
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 ">John Doe</h1>
       <p className="text-sm text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
