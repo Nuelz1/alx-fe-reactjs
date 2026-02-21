47021af00be7a9b6f7c20e9bf3c7ac827515026a
Osho Emmanuel <oshoemmanuel3@gmail.com>
2026-02-21 14:40:48 +0100

    Added the missen syntax in tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Paths to all your React components
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
